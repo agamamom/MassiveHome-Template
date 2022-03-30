e8dda28059995b208c355fc42e4d1af4832c73a6
Dat <dathoang31082001@gmail.com>
2022-03-30 11:34:25 +0700

    update banner responsive

--- js/main.js
+++ js/main.js
@@ -19,3 +19,20 @@ function clickHandle(evt, animalName) {
     document.getElementById(animalName).style.display = "block";
     evt.currentTarget.className += " active";
 }
+
+window.onscroll = () => {
+
+    if (window.scrollY > 0) {
+        document.querySelector('.header').classList.add('active');
+    } else {
+        document.querySelector('.header').classList.remove('active');
+    }
+};
+
+window.onload = () => {
+    if (window.scrollY > 0) {
+        document.querySelector('.header').classList.add('active');
+    } else {
+        document.querySelector('.header').classList.remove('active');
+    }
+};
